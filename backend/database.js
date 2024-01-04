d95f40e0105a9465a0475947b8d03131b70a8cca
George Townley <george@knowlesavenue.com>
2024-01-04 11:56:26 +0000

    initialise SQLite database

---
+++ backend/database.js
@@ -0,0 +1,13 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(
+  "./jobTrackerDB.sqlite",
+  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the SQLite database.");
+  }
+);
+
+module.exports = db;
