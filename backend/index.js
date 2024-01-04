7411393adf7c41f0c7d53aa146d175858460e971
George Townley <george@knowlesavenue.com>
2024-01-04 11:58:41 +0000

    define api endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./database");
 
 const app = express();
 const port = 3001; // Has to be on a different port than the react front end
@@ -13,4 +14,21 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+app.post("/applications", (req, res) => {
+  const { employer, dateApplied, platform, progress, workType, pay } = req.body;
+  const sql = `INSERT INTO job_applications (employer, date_applied, platform, progress, work_type, pay) VALUES (?, ?, ?, ?, ?, ?)`;
+
+  db.run(
+    sql,
+    [employer, dateApplied, platform, progress, workType, pay],
+    function (err) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({ id: this.lastID });
+    }
+  );
 });
