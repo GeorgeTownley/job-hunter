7b128334abe11cfa6e04b09fd180239086e8c296
George Townley <george@knowlesavenue.com>
2024-01-09 20:27:01 +0000

    remove user+pass login

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -1,30 +1,17 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   providers: [
-    Providers.GitHub({
+    GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    Providers.Credentials({
-      // Implement custom sign in logic here
-      authorize: async (credentials) => {
-        // You should replace this with a real database check
-        if (
-          credentials.username === "user" &&
-          credentials.password === "password"
-        ) {
-          return Promise.resolve({
-            id: 1,
-            name: "User",
-            email: "user@example.com",
-          });
-        } else {
-          return Promise.resolve(null);
-        }
-      },
-    }),
   ],
-  // Configure NextAuth options here
+  pages: {
+    signIn: "/signin", // point to your custom sign-in page
+  },
+  callbacks: {
+    redirect: async (url, baseUrl) => baseUrl,
+  },
 });
