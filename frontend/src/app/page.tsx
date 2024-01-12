dc17a70075c52784fea91fe208a777cf1a3daa3d
George Townley <george@knowlesavenue.com>
2024-01-12 16:55:26 +0000

    add additional styling for sign in page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,15 +11,41 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          console.log("clicked!");
-          signIn("github", { callbackUrl: "http://localhost:3000/homepage" });
-        }}
-      >
-        Sign in with GitHub
-      </button>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+      <div className="max-w-md w-full space-y-8 p-10 bg-white shadow-lg rounded-xl">
+        <div className="text-center">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Or{" "}
+            <a
+              href="#"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              start your 14-day free trial
+            </a>
+          </p>
+        </div>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <button
+                onClick={() => {
+                  console.log("clicked!");
+                  signIn("github", {
+                    callbackUrl: "http://localhost:3000/homepage",
+                  });
+                }}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign in with GitHub
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
