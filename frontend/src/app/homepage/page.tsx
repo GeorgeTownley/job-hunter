eb8baea07ad711695b9ece9f5130fd66e770dbf3
George Townley <george@knowlesavenue.com>
2024-01-12 16:55:40 +0000

    color match buttons

--- frontend/src/app/homepage/page.tsx
+++ frontend/src/app/homepage/page.tsx
@@ -199,7 +199,7 @@ export default function HomePage() {
         </div>
         <button
           type="submit"
-          className="px-4 py-2  mt-4   bg-blue-500 text-white rounded-md hover:bg-blue-700"
+          className="px-4 py-2  mt-4   bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
         >
           Submit
         </button>
