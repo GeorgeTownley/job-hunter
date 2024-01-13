98cec23989ecd8172a2c47e261126a3d5cb72e64
George Townley <george@knowlesavenue.com>
2024-01-13 12:18:18 +0000

    add Navbar component

--- frontend/src/app/homepage/page.tsx
+++ frontend/src/app/homepage/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import Navbar from "@/components/Navbar";
 
 type Application = {
   id: number;
@@ -145,216 +146,219 @@ export default function HomePage() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto my-8">
-        <div className="flex flex-col space-y-4">
-          <input
-            type="text"
-            name="employer"
-            value={formData.employer}
-            onChange={handleChange}
-            placeholder="Employer"
-            className="px-4 py-2 border rounded-md"
-          />
-          <input
-            type="date"
-            name="date_applied"
-            value={formData.date_applied}
-            onChange={handleChange}
-            placeholder="Date Applied"
-            className="px-4 py-2 border rounded-md"
-          />
-          <input
-            type="text"
-            name="platform"
-            value={formData.platform}
-            onChange={handleChange}
-            placeholder="Platform"
-            className="px-4 py-2 border rounded-md"
-          />
-          <input
-            type="text"
-            name="progress"
-            value={formData.progress}
-            onChange={handleChange}
-            placeholder="Progress"
-            className="px-4 py-2 border rounded-md"
-          />
-          <input
-            type="text"
-            name="work_type"
-            value={formData.work_type}
-            onChange={handleChange}
-            placeholder="Work type"
-            className="px-4 py-2 border rounded-md"
-          />
-          <input
-            type="number"
-            name="pay"
-            value={formData.pay}
-            onChange={handleChange}
-            placeholder="Pay"
-            className="px-4 py-2 border rounded-md"
-          />
-        </div>
-        <button
-          type="submit"
-          className="px-4 py-2  mt-4   bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
-        >
-          Submit
-        </button>
-      </form>
-      <div className="w-4/5 mx-auto">
-        <table className="min-w-full divide-y divide-gray-200 shadow-sm table-fixed w-full">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Employer
-              </th>
-              <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Date Applied
-              </th>
-              <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Platform
-              </th>
-              <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Progress
-              </th>
-              <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Work Type
-              </th>
-              <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Pay
-              </th>
-              <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {applications.map((application) => (
-              <tr key={application.id}>
-                <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
-                  {editingId === application.id ? (
-                    <input
-                      className="text-center"
-                      type="text"
-                      name="employer"
-                      value={editableData.employer}
-                      onChange={handleEditChange}
-                    />
-                  ) : (
-                    application.employer
-                  )}
-                </td>
-                <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
-                  {editingId === application.id ? (
-                    <input
-                      className="text-center"
-                      type="date"
-                      name="date_applied"
-                      value={editableData.date_applied}
-                      onChange={handleEditChange}
-                      onBlur={handleEditChange}
-                    />
-                  ) : (
-                    application.date_applied
-                  )}
-                </td>
-                <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
-                  {editingId === application.id ? (
-                    <input
-                      className="text-center"
-                      type="text"
-                      name="platform"
-                      value={editableData.platform}
-                      onChange={handleEditChange}
-                    />
-                  ) : (
-                    application.platform
-                  )}
-                </td>
-                <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
-                  {editingId === application.id ? (
-                    <input
-                      className="text-center"
-                      type="text"
-                      name="progress"
-                      value={editableData.progress}
-                      onChange={handleEditChange}
-                    />
-                  ) : (
-                    application.progress
-                  )}
-                </td>
-                <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
-                  {editingId === application.id ? (
-                    <input
-                      className="text-center"
-                      type="text"
-                      name="work_type"
-                      value={editableData.work_type}
-                      onChange={handleEditChange}
-                    />
-                  ) : (
-                    application.work_type
-                  )}
-                </td>
-                <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
-                  {editingId === application.id ? (
-                    <input
-                      className="text-center"
-                      type="number"
-                      name="pay"
-                      value={editableData.pay}
-                      onChange={handleEditChange}
-                    />
-                  ) : (
-                    new Intl.NumberFormat("en-US").format(application.pay)
-                  )}
-                </td>
-
-                <td>
-                  <div className="flex px-1 py-2 justify-center items-center space-x-3">
-                    {editingId === application.id ? (
-                      // Render the Save button only if we're in edit mode for this row
-                      <button
-                        className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-700"
-                        onClick={() => handleSave(application.id)}
-                      >
-                        Save
-                      </button>
-                    ) : (
-                      // Otherwise, render the Edit button which is always visible
-                      <button
-                        className="px-4 py-2  bg-yellow-500 text-white rounded-md hover:bg-yellow-700"
-                        onClick={() => handleEdit(application)}
-                      >
-                        Edit
-                      </button>
-                    )}
-                    {deletingId === application.id ? (
-                      <button
-                        className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-700"
-                        onClick={() => handleConfirmDelete(application.id)}
-                      >
-                        Confirm
-                      </button>
-                    ) : (
-                      <button
-                        className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-700"
-                        onClick={() => setDeletingId(application.id)}
-                      >
-                        Delete
-                      </button>
-                    )}
-                  </div>
-                </td>
+    <>
+      <Navbar />
+      <div>
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto my-8">
+          <div className="flex flex-col space-y-4">
+            <input
+              type="text"
+              name="employer"
+              value={formData.employer}
+              onChange={handleChange}
+              placeholder="Employer"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="date"
+              name="date_applied"
+              value={formData.date_applied}
+              onChange={handleChange}
+              placeholder="Date Applied"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="text"
+              name="platform"
+              value={formData.platform}
+              onChange={handleChange}
+              placeholder="Platform"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="text"
+              name="progress"
+              value={formData.progress}
+              onChange={handleChange}
+              placeholder="Progress"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="text"
+              name="work_type"
+              value={formData.work_type}
+              onChange={handleChange}
+              placeholder="Work type"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="number"
+              name="pay"
+              value={formData.pay}
+              onChange={handleChange}
+              placeholder="Pay"
+              className="px-4 py-2 border rounded-md"
+            />
+          </div>
+          <button
+            type="submit"
+            className="px-4 py-2  mt-4   bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
+          >
+            Submit
+          </button>
+        </form>
+        <div className="w-4/5 mx-auto">
+          <table className="min-w-full divide-y divide-gray-200 shadow-sm table-fixed w-full">
+            <thead className="bg-gray-50">
+              <tr>
+                <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Employer
+                </th>
+                <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Date Applied
+                </th>
+                <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Platform
+                </th>
+                <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Progress
+                </th>
+                <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Work Type
+                </th>
+                <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Pay
+                </th>
+                <th className="px-6 py-3 text-middle text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Actions
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {applications.map((application) => (
+                <tr key={application.id}>
+                  <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
+                    {editingId === application.id ? (
+                      <input
+                        className="text-center"
+                        type="text"
+                        name="employer"
+                        value={editableData.employer}
+                        onChange={handleEditChange}
+                      />
+                    ) : (
+                      application.employer
+                    )}
+                  </td>
+                  <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
+                    {editingId === application.id ? (
+                      <input
+                        className="text-center"
+                        type="date"
+                        name="date_applied"
+                        value={editableData.date_applied}
+                        onChange={handleEditChange}
+                        onBlur={handleEditChange}
+                      />
+                    ) : (
+                      application.date_applied
+                    )}
+                  </td>
+                  <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
+                    {editingId === application.id ? (
+                      <input
+                        className="text-center"
+                        type="text"
+                        name="platform"
+                        value={editableData.platform}
+                        onChange={handleEditChange}
+                      />
+                    ) : (
+                      application.platform
+                    )}
+                  </td>
+                  <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
+                    {editingId === application.id ? (
+                      <input
+                        className="text-center"
+                        type="text"
+                        name="progress"
+                        value={editableData.progress}
+                        onChange={handleEditChange}
+                      />
+                    ) : (
+                      application.progress
+                    )}
+                  </td>
+                  <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
+                    {editingId === application.id ? (
+                      <input
+                        className="text-center"
+                        type="text"
+                        name="work_type"
+                        value={editableData.work_type}
+                        onChange={handleEditChange}
+                      />
+                    ) : (
+                      application.work_type
+                    )}
+                  </td>
+                  <td className="px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900">
+                    {editingId === application.id ? (
+                      <input
+                        className="text-center"
+                        type="number"
+                        name="pay"
+                        value={editableData.pay}
+                        onChange={handleEditChange}
+                      />
+                    ) : (
+                      new Intl.NumberFormat("en-US").format(application.pay)
+                    )}
+                  </td>
+
+                  <td>
+                    <div className="flex px-1 py-2 justify-center items-center space-x-3">
+                      {editingId === application.id ? (
+                        // Render the Save button only if we're in edit mode for this row
+                        <button
+                          className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-700"
+                          onClick={() => handleSave(application.id)}
+                        >
+                          Save
+                        </button>
+                      ) : (
+                        // Otherwise, render the Edit button which is always visible
+                        <button
+                          className="px-4 py-2  bg-yellow-500 text-white rounded-md hover:bg-yellow-700"
+                          onClick={() => handleEdit(application)}
+                        >
+                          Edit
+                        </button>
+                      )}
+                      {deletingId === application.id ? (
+                        <button
+                          className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-700"
+                          onClick={() => handleConfirmDelete(application.id)}
+                        >
+                          Confirm
+                        </button>
+                      ) : (
+                        <button
+                          className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-700"
+                          onClick={() => setDeletingId(application.id)}
+                        >
+                          Delete
+                        </button>
+                      )}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import { signOut, useSession } from "next-auth/react";
+
+const Navbar = () => {
+  const { data: session } = useSession();
+
+  const handleLogout = async () => {
+    await signOut({ callbackUrl: "/" });
+  };
+
+  // Use optional chaining and provide fallback values
+  const userProfileImage = session?.user?.image ?? "/default-profile.png";
+  const userName = session?.user?.name ?? "User";
+
+  return (
+    <nav className="bg-white shadow">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          {/* Left side content of navbar */}
+          <div className="flex-1 flex items-center justify-start">
+            <div className="flex-shrink-0">
+              <img className="h-8 w-8" src="/your-logo.png" alt="Your Logo" />
+            </div>
+            {/* Additional content can go here */}
+          </div>
+
+          {/* Right side content of navbar */}
+          {session?.user && (
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="flex items-center">
+                <img
+                  className="h-8 w-8 rounded-full"
+                  src={userProfileImage}
+                  alt={userName}
+                />
+                <button
+                  onClick={handleLogout}
+                  className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-700 bg-gray-200 hover:bg-gray-300"
+                >
+                  Log out
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
